7def1dce1305a5c362fd334c18a5cad9de94612f
JeppeLovstad <jeppemlv@gmail.com>
2021-05-31 13:21:07 +0200

    1b done

--- Part 1/src/App.js
+++ Part 1/src/App.js
@@ -20,30 +20,30 @@ const Header = (props) => {
 const Part1 = (props) => {
   return (
     <div>
-      <p>{props.part1} {props.exercises1}</p>
+      <p>{props.part} {props.exercise}</p>
     </div>)
 }
 
 const Part2 = (props) => {
   return (
     <div>
-      <p>{props.part2} {props.exercises2}</p>
+      <p>{props.part} {props.exercise}</p>
     </div>)
 }
 
 const Part3 = (props) => {
   return (
     <div>
-      <p>{props.part3} {props.exercises3}</p>
+      <p>{props.part} {props.exercise}</p>
     </div>)
 }
 
 const Content = (props) => {
   return (
     <div>
-      < Part1 part1={props.part1} exercises1={props.exercises1}/>
-      < Part2 part2={props.part2} exercises2={props.exercises2}/>
-      < Part3 part3={props.part3} exercises3={props.exercises3}/>
+      < Part1 part={props.parts[0].name} exercise={props.parts[0].exercise}/>
+      < Part2 part={props.parts[1].name} exercise={props.parts[1].exercise}/>
+      < Part3 part={props.parts[2].name} exercise={props.parts[2].exercise}/>
     </div>
   )
 }
@@ -51,7 +51,7 @@ const Content = (props) => {
 const Total = (props) => {
   return (
     <div>
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+      <p>Number of exercises {props.parts[0].exercise + props.parts[1].exercise + props.parts[2].exercise}</p>
     </div>
   )
 }
@@ -60,14 +60,12 @@ function App() {
   const now = new Date()
   const a = Math.random()
   const b = 20
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
+  const course = {name:'Half Stack application development',
+   parts:[
+                {name:'Fundamentals of React', exercise:10},
+                {name:'Using props to pass data', exercise:7},
+                {name:'State of a component', exercise:14}]
+   }
   return (
     <div className="App">
       <header className="App-header">
@@ -76,9 +74,9 @@ function App() {
           {now.toUTCString()} <br/>
           {a} + {b} = {a + b}
         </p>
-      < Header course={course}/>
-      < Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
-      < Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      < Header course={course.name}/>
+      < Content parts={course.parts} />
+      < Total parts={course.parts}/>
       </header>
     </div>
   );
